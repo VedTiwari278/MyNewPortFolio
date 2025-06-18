24c0ac988097629fe667d4f2cd5a1999b77866e0
VedTiwari278 <148948448+VedTiwari278@users.noreply.github.com>
2025-06-18 16:46:06 +0530

    Update NavBar.jsx

--- FRONTEND/src/assets/NavBar.jsx
+++ FRONTEND/src/assets/NavBar.jsx
@@ -56,7 +56,7 @@ const NavBar = () => {
                   : ""
               }`}
             >
-              DashBioard
+              DashBoard
             </Nav.Link>
 
             <Nav.Link
